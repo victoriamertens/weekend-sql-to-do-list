5013a873bdcc8a8129a761117a62df0d8b98ac66
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-12 08:30:05 -0600

    Completed PUT Request in server

--- server/router.tasklist.js
+++ server/router.tasklist.js
@@ -19,12 +19,6 @@ res.send(result.rows);
   res.sendStatus(500); 
 })
 })
-//take in url without anything behind it 
-//write the queryText with SELECT
-//SANITIZE the data 
-//send the query text with pg pool 
-//create the response to client.js 
-
 
 //POST Request to Database 
 router.post('/', (req, res)=>{ 
@@ -48,10 +42,23 @@ res.sendStatus(201);
 //PUT Request to Database 
 
 //take in url with id in as param
-//write the queryText with UPDATE 
-//SANITIZE the data 
-//send the query text with pg pool 
-//create the response to client.js 
+router.put('/:id', (req, res)=>{ 
+  console.log("In PUT request", req.params.id); 
+  let id = req.params.id; 
+  let queryText = `
+  UPDATE "tasks" 
+  SET "completed" = 'true'
+  WHERE "id" = $1
+  `;
+pool.query(queryText, [id]).then((result)=> {
+console.log("completed put", result); 
+res.sendStatus(200);
+}).catch((error)=>{ 
+  console.log(error); 
+  res.sendStatus(500); 
+})
+})
+
 
 
 //DELETE Request to Database 
