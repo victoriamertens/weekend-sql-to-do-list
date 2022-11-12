be159fbe3db8465cf075d289b2312751dd2fe82a
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-12 08:55:43 -0600

    Client plan commented out

---
+++ server/public/client.js
@@ -0,0 +1,32 @@
+//Document ready function 
+
+//Function onReady 
+//run clickListeners() function 
+// run getData();
+
+
+
+//Function clickListeners(){}
+//$(addBtn).on('click', addTask);
+//$(CHECKBOX PARENT).on('click', completeTask);
+//$(DELETE PARENT).on('click',deleteTask);
+
+//GET FUNCTION 
+//function getData(){}
+//ajax GET, url: /task_list
+//renderDOM(); 
+
+//Render Function 
+//function renderDOM(){}
+
+//POST FUNCTION 
+//function addTask(){}
+//ajax POST, data: {task: input}
+
+//PUT FUNCTION 
+//function completeTask(){}
+//ajax PUT, url: /task_list/id
+
+//DELETE FUNCTION 
+//function deleteTask(){}
+//ajax DELETE, url: /task_list/id
