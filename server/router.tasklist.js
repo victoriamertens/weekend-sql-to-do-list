c61c21fd18f5388b96b2f388234d19b8e9ff9877
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-12 08:19:10 -0600

    POST Request on server complete, updated sql file

--- server/router.tasklist.js
+++ server/router.tasklist.js
@@ -1,8 +1,8 @@
 let express = require('express'); 
-let router = epress.Router(); 
+let router = express.Router(); 
 
 //use the pool export to run the requests
-
+let pool = require('./modules/pool.js'); 
 
 //GET Request to Database 
 
@@ -14,7 +14,23 @@ let router = epress.Router();
 
 
 //POST Request to Database 
-
+router.post('/', (req, res)=>{ 
+  console.log("In POST request", req.body); 
+  let input = req.body;
+  let queryText = `
+  INSERT INTO "tasks" 
+  ("task")
+  VALUES
+  ($1)
+  `;
+pool.query(queryText, [input.task]).then((result)=> {
+console.log("completed post"); 
+res.sendStatus(201);
+}).catch((error)=>{ 
+  console.log(error); 
+  res.sendStatus(500); 
+})
+})
 //take in url without anything behind it 
 //write the queryText with INSERT INTO
 //SANITIZE the data 
