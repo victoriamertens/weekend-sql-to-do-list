705284d9d2bdb581ef9d631ee8679472acdb7a44
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-11 19:12:49 -0600

    Wrote the postgresql pool file

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
+//Set Port
+const PORT = 5000;
+//Inport express
 const express = require('express');
-
 const app = express();
+//Import BodyParser 
 const bodyParser = require('body-parser');
- const PORT =   5000;
-//const taskRouter = require('./router.tasklist.js')
-
 app.use(bodyParser.urlencoded({extended: true}));
+//Serve Static Files 
 app.use(express.static('server/public'));
 //process.env.PORT
-
+//const taskRouter = require('./router.tasklist.js')
 
 // ROUTES
 //app.use('/task_list', taskRouter);
+
+
+
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
