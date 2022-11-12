b777d8ff6466f1d787007fa6d9e6d624f0dc0745
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-11 18:45:42 -0600

    Server up and running

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const taskRouter = express.Router();
 
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
-const taskRouter = require('server/router.tasklist.js')
+ const PORT =   5000;
+//const taskRouter = require('./router.tasklist.js')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
-
+//process.env.PORT
 
 
 // ROUTES
-app.use('/task_list', taskRouter);
+//app.use('/task_list', taskRouter);
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
