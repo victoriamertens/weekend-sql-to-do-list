a813d4cbb03452f84b4c923b9c9dc2fa70de2064
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-14 15:48:27 -0600

    Updated for heroku and bit.io

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,18 @@
 //Create Pool for PG 
 const pg = require('pg'); 
 const Pool = pg.Pool; 
-const pool = new Pool({
+let pool; 
+if(process.env.DATABASE_URL){ 
+  pool = new Pool({connectionString: process.env.DATABASE_URL, 
+                    ssl:{rejectUnauthorized: false}}); 
+} else {
+pool = new Pool({
   database: 'weekend_to_do_app',
   host: 'localhost', 
   port: 5432, 
   max: 10, 
   idleTimeoutMillis: 30000, 
-}); 
+});} 
 pool.on('connect', ()=>{ 
   console.log('PostgreSQL is connected!');
 })
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 //Set Port
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 //Inport express
 const express = require('express');
 const app = express();
@@ -9,13 +9,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Serve Static Files 
 app.use(express.static('server/public'));
 
-
-
 // ROUTES
-const taskRouter = require('./router.tasklist.js')
-app.use('/task_list', taskRouter);
-
-
+//const taskRouter = require('./router.tasklist.js')
+//app.use('/task_list', taskRouter);
 
 
 // Start listening for requests on a specific port
