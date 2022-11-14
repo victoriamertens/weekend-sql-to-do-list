dd601820733cf16d49c8f9a37e63c1b5b00b7c5f
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-14 16:06:10 -0600

    Fixed problem

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Serve Static Files 
 app.use(express.static('server/public'));
 
-// ROUTES
-//const taskRouter = require('./router.tasklist.js')
-//app.use('/task_list', taskRouter);
+//ROUTES
+const taskRouter = require('./router.tasklist.js')
+app.use('/task_list', taskRouter);
 
 
 // Start listening for requests on a specific port
